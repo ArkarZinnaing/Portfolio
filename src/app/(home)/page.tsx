bad2d5c621922a0a6417dbc4d671c8fa9f253a84
Saw-Pyae-Sone <sawpyaesone23@gmail.com>
2025-01-03 17:50:04 +0700

    final commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import React from "react";
 import Hero from "../components/hero";
 import About from "../components/About";
 import Skills from "../components/Skills";
@@ -7,48 +7,17 @@ import Work from "../components/Work";
 import Project from "../components/Project";
 import Contact from "../components/Contact";
 
-// Scroll
-import { ReactLenis } from 'lenis/react';
-import React, { useEffect } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
-
-export default function Page() {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const elements = gsap.utils.toArray(".reveal-up");
-
-      elements.forEach((element) => {
-        if (element instanceof HTMLElement) {
-          gsap.to(element, {
-            scrollTrigger: {
-              trigger: element,
-              start: "-200 bottom",
-              end: "bottom 80%",
-              scrub: true,
-            },
-            y: 0,
-            opacity: 1,
-            duration: 1,
-            ease: "power2.out",
-          });
-        }
-      });
-    }
-  }, []);
-
-  return (
-    <ReactLenis root>
+export default function page(){
+  return(
+    <>
       <main>
-        <Hero />
-        <About />
-        <Skills />
+        <Hero/>
+        <About/>
+        <Skills/>
         <Work />
         <Project />
         <Contact />
       </main>
-    </ReactLenis>
-  );
+    </>
+  )
 }

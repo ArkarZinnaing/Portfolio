42a9c82ec06e3f2cdfe1de2fe18c794e2546e15b
Saw-Pyae-Sone <sawpyaesone23@gmail.com>
2025-01-06 22:57:56 +0700

    changes

--- src/app/components/Work.tsx
+++ src/app/components/Work.tsx
@@ -5,7 +5,7 @@ import { Timeline } from "@/ui/timeline";
 export default function Work() {
   const data = [
     {
-      title: "2024",
+      title: "2025",
       content: (
         <div>
           <h4 className="headline-2 mb-5">Front-End Engineer</h4>
@@ -64,14 +64,74 @@ export default function Work() {
       ),
     },
     {
-      title: "Early 2023",
+      title: "Late 2024",
       content: (
         <div>
+          <h4 className="headline-2 mb-5">Front-End Developer Internship</h4>
           <p className="text-zinc-50 dark:text-neutral-200 text-sm md:text-base font-normal mb-5">
-            I usually run out of copy, but when I see content this big, I try to
-            integrate lorem ipsum.
+            As a freelancer, I am currently working on a food ordering website for the Burmese Restaurant called Burmese Bites
           </p>
-          <p className="text-zinc-50 dark:text-neutral-200 text-sm md:text-sm font-normal mb-8">September 2024 - Persent </p>
+          <p className="text-zinc-50 dark:text-neutral-200 text-sm md:text-sm font-normal mb-5"><strong>Duration:</strong> October 2024 - Persent </p>
+          <div className="mb-8">
+            <div className="flex gap-2 items-center text-zinc-300 dark:text-neutral-300 text-xs md:text-sm">
+              📌 Card grid component
+            </div>
+            <div className="flex gap-2 items-center text-zinc-700 dark:text-neutral-300 text-xs md:text-sm">
+              📌 Startup template Aceternity
+            </div>
+            <div className="flex gap-2 items-center text-zinc-700 dark:text-neutral-300 text-xs md:text-sm">
+              📌 Random file upload lol
+            </div>
+            <div className="flex gap-2 items-center text-zinc-700 dark:text-neutral-300 text-xs md:text-sm">
+              📌 Himesh Reshammiya Music CD
+            </div>
+            <div className="flex gap-2 items-center text-zinc-700 dark:text-neutral-300 text-xs md:text-sm">
+              📌 Salman Bhai Fan Club registrations open
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/1.png"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/2.png"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/3.png"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            <Image
+              src="/4.png"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "Early 2024",
+      content: (
+        <div>
+          <h4 className="headline-2 mb-5">University of Central Lancashire</h4>
+          <p className="text-zinc-50 dark:text-neutral-200 text-sm md:text-base font-normal mb-5">
+            Bachelor of Science (B.Sc), First Class Honours, Business Computing and Information Systems 
+          </p>
+          <p className="text-zinc-50 dark:text-neutral-200 text-sm md:text-sm font-normal mb-5"><strong>GPA:</strong> 3.7/4.0 </p>
+          <p className="text-zinc-50 dark:text-neutral-200 text-sm md:text-sm font-normal mb-8"><strong>Duration:</strong> September 2019 - March 2024 </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
               src="https://assets.aceternity.com/pro/hero-sections.png"
